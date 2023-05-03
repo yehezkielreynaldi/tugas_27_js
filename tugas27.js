dfa247fc3d259a1fc2a8a38b501c62a2636cfa58
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-05-03 21:05:45 +0700

    Tugas 27 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,18 @@
+function mainString() {
+
+    let a = "No ";
+    let b = "rumah saya";
+    let c = 30;
+    let d = "A";
+    let kalimat1 = a + b + " " + c + d;
+    console.log(kalimat1);
+
+    let e = "Jalan pangeran Tirtayasa No"
+    let f = 70;
+
+    let kalimat2 = e + f;
+    console.log(kalimat2);
+
+}
+
+mainString();
